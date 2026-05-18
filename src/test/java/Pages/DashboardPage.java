477e43d06c2c137902e91a10f2fc6a86b25f2c18
ikondo76 <ignatiuskondo@gmail.com>
2026-05-18 21:18:15 +0200

    modifiedstepsDef and added DashboardPage

---
+++ src/test/java/Pages/DashboardPage.java
@@ -0,0 +1,33 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
+public class DashboardPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//*[@id=\"app-main-content\"]/section/div[1]/h2")
+    WebElement dashboardHeading_xpath;
+
+    @FindBy(xpath = "//*[@id=\"app-root\"]/nav/div[1]/div[3]/div/button")
+    WebElement userMenuButton_xpath;
+
+    public DashboardPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyDashboardPageIsDisplayed() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(dashboardHeading_xpath));
+        dashboardHeading_xpath.isDisplayed();
+    }
+
+    public void clickUserMenuButton_xpath() {
+        userMenuButton_xpath.click();
+    }
+}
--- src/test/java/Steps/stepsDef.java
+++ src/test/java/Steps/stepsDef.java
@@ -1,30 +1,36 @@
 package Steps;
 
-public class stepsDef {
+import Pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class stepsDef extends Base {
     @Given("i am on the login page")
     public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        homePage.clickLoginButton();
+        loginPage.verifyLoginPageIsDisplayed();
     }
-    @Given("i enter email password")
-    public void i_enter_email_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Given("i enter email email")
+    public void i_enter_email(String email) {
+        loginPage.enterEmail(email);
+
     }
-    @Given("i enter password <password>")
-    public void i_enter_password_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @Given("i enter password")
+    public void i_enter_password(String password) {
+        loginPage.enterPassword(password);
+
     }
-    @When("i clicklogin button")
-    public void i_clicklogin_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @When("i click login Submit button")
+    public void i_click_login_button() {
+        loginPage.loginSubmitButton();
     }
+
     @Then("i should be logged in successfully")
     public void i_should_be_logged_in_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
     }
 
 }
