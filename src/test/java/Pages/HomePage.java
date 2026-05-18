080d02c1eceeadc09730f5b245e8db494958157b
ikondo76 <ignatiuskondo@gmail.com>
2026-05-18 20:43:16 +0200

    added LoginPage methods

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,4 +1,26 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 public class HomePage {
+    WebDriver driver;
+
+    @FindBy(xpath ="//*[@id=\"app-root\"]/nav/div[1]/div[3]/button/span[2]")
+    WebElement loginButton_xpath;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginButton_xpath));
+        loginButton_xpath.click();
+    }
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,53 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
+
 public class LoginPage {
+    WebDriver driver;
+
+    @FindBy(xpath ="//*[@id=\"login-heading\"]/span")
+    WebElement loginHeading_xpath;
+
+    @FindBy(id = "login-email")
+    WebElement loginEmail_id;
+
+    @FindBy(id = "login-password")
+    WebElement loginPassword_id;
+
+    @FindBy(id = "login-submit")
+    WebElement loginSubmitButton_id;
+
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyLoginPageIsDisplayed() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginHeading_xpath));
+        loginHeading_xpath.isDisplayed();
+    }
+
+    public void enterEmail(String email) {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginEmail_id));
+        loginEmail_id.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPassword_id));
+        loginPassword_id.sendKeys(password);
+    }
+
+    public void loginSubmitButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(15)).until(visibilityOf(loginPassword_id));
+        loginSubmitButton_id.click();
+    }
+
+
 }
